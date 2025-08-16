9f372142407b8173f54a13020a5b9bf6db857954
Juthuka Dharshan Amesh <dharshanamesh04@gmail.com>
2025-08-16 16:32:09 +0530

    Initial commit

---
+++ highscores.js
@@ -0,0 +1,9 @@
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores
+  .sort((a, b) => b.score - a.score) // highest first
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
